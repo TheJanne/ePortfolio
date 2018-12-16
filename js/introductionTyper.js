6747e80f4fe70959aee0847b3fead0d9559e0189
TheJanne <janne.tuhkanen@hotmail.com>
2018-12-16 17:29:51 +0200

    Cleaned up some code.

--- js/introductionTyper.js
+++ js/introductionTyper.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     
     let introductionText = "Greetings! My name is Janne, I am a web-application developer from Oulu, Finland!";
-    let welcomeText = "This is my portfolio. Welcome!"; 
+    let welcomeText = "This is my portfolio of my studies in DevLAB. Welcome!"; 
 
     new Typed('#greeting', {
         strings: [introductionText, welcomeText],
         typeSpeed: 20,
         backDelay: 1500,
-        loop: true,
+        loop: true
     });
 });
